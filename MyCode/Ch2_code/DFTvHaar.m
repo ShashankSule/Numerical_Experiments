254dc7259a99eb853117a726172502337cb3dd2a
Shashank Sule <shashanksule8@gmail.com>
2020-03-18 11:06:57 -0400

    New code
    Some visualization for Chapter 2

---
+++ MyCode/Ch2_code/DFTvHaar.m
@@ -0,0 +1,47 @@
+Img = zeros(32,32);
+Img(16,16) = 1;
+Img(16,17) = 1;
+Img(17,16) = 1;
+Img(17,17) = 1;
+
+
+FT = fft2(Img);
+[a,h,v,d] = haart2(Img, 'integer');
+
+HT = cell2mat(d(1));
+
+figure();
+
+
+subplot(2,2,1);
+imagesc(Img);
+caxis('manual');
+caxis([-1 1])
+title('Original Image','interpreter','latex');
+set(gca,'XColor', 'none','YColor','none')
+
+
+subplot(2,2,2);
+imagesc(HT);
+caxis('manual');
+caxis([-1 1])
+title('16-point Haar Coefficients','interpreter','latex');
+set(gca,'XColor', 'none','YColor','none')
+
+
+subplot(2,2,3);
+imagesc(real(FT));
+caxis('manual');
+caxis([-1 1])
+title('Real part of Fourier coefficients','interpreter','latex');
+set(gca,'XColor', 'none','YColor','none')
+
+
+subplot(2,2,4);
+imagesc(imag(FT));
+caxis('manual');
+caxis([-1 1])
+title('Imaginary part of Fourier coefficients','interpreter','latex');
+set(gca,'XColor', 'none','YColor','none')
+
+
