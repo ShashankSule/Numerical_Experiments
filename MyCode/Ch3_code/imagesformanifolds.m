667e314ec22e1b5647f4c431725a25a31abb872d
Shashank Sule <shashanksule8@gmail.com>
2020-04-01 01:26:26 -0400

    More manifold code
    Afonso Bandeira's experiment

--- MyCode/Ch3_code/imagesformanifolds.m
+++ MyCode/Ch3_code/imagesformanifolds.m
@@ -1,20 +1,31 @@
 figure;
-N = 40;
-data_matrix = zeros(256,N);
+N = 16;
+data_matrix = zeros(1024,3,N);
 for i=1:N
-    %z = randi([1 256],1,1);
-    M = zeros(16,16);
-    column = rem(i-1,16) + 1;
-    M(7, column) = 1;
-    data_matrix(:,i) = M(:);
-    subplot(8,5,i);
+    % Generate plot
+    z = randi([1 1024],1,1);
+    M = zeros(32,32);
+    col = rem(z-1,32) + 1;
+    row = ceil(z/32);
+    M(row, col) = 1;
+    subplot(4,4,i);
     imagesc(M);
+    
+    %Make the data
+    M = M';
+    data_matrix(:,1,i) = M(:);
+    for m=1:1024
+            row = ceil(m/32);
+            column = rem(m-1,32)+1;
+            data_matrix(m,2,i) = row/32;
+            data_matrix(m,3,i) = column/32;
+    end
 end
-eps = 0.5;
+eps = 5;
 weight_matrix = zeros(N,N);
 for i = 1:N
     for j = 1:N
-        u = (norm(data_matrix(:,i) - data_matrix(:,j)))^2;
+        u = (norm(data_matrix(:,:,i) - data_matrix(:,:,j)))^2;
         weight_matrix(i,j) = exp(-(1/(2*eps))*u);
     end
 end
